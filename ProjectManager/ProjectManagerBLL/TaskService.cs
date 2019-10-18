8bfd6b7a989c83ceee3d003ec8bf761bd8a37998
unknown <Administrator@ECLASSROOM-08.corp.hexaware.com>
2019-10-18 13:26:43 +0530

    added project repo

---
+++ ProjectManager/ProjectManagerBLL/TaskService.cs
@@ -0,0 +1,68 @@
+﻿using Exceptions;
+using ProjectManagerDAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace ProjectmanagerBLL
+{
+    public class TaskService
+    {
+        static TaskNRepository TaskRepo = null;
+        public TaskService()
+        {
+            TaskRepo = new TaskNRepository();
+        }
+        public List<TaskN> Display()
+        {
+            try
+            {
+                return TaskRepo.Display().ToList();
+            }
+            catch (ProjectManagerException e)
+            {
+                throw e;
+            }
+        }
+        public bool AddTask(TaskN proj)
+        {
+            try
+            {
+                return TaskRepo.Add(proj);
+            }
+            catch (ProjectManagerException e)
+            {
+                throw e;
+            }
+        }
+
+        public List<TaskN> GetTasks(int projectId)
+        {
+
+            try
+            {
+                return TaskRepo.GetTasks(projectId);
+            }
+            catch (Exception ex)
+            {
+                throw new ProjectManagerException("Error finding Task" + ex);
+            }
+        }
+        public List<Employee> Displaypendingtasks()
+        {
+            try
+            {
+                return TaskRepo.Displaypendingtasks().ToList();
+            }
+            catch (ProjectManagerException e)
+            {
+                throw e;
+            }
+        }
+
+
+    }
+}
